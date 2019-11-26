e2925480c297dd751cde8efb844e7e78d649c7ea
Badrul Alam <hridoyjbd@gmail.com>
2019-11-26 20:58:12 +0600

    SA1009 API created

--- src/main/java/com/nahalit/nahalapimanager/model/SaSubregion.java
+++ src/main/java/com/nahalit/nahalapimanager/model/SaSubregion.java
@@ -1,16 +1,18 @@
 package com.nahalit.nahalapimanager.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Setter
 @Getter
 @Entity
 public class SaSubregion {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SA_SUBREGION")
+  @SequenceGenerator(sequenceName = "S_SA_SUBREGION", allocationSize = 1, name = "SA_SUBREGION")
   private Long subregionNo;
   private Long regionNo;
   private String subregionName;
